c5cbafeee2f467c895bd73438568f2634687bb75
benhur feld <benhurfeld@gmail.com>
2024-04-18 15:22:39 -0300

    proxy :/

--- run.py
+++ run.py
@@ -6,13 +6,13 @@ import sys
 model_path = "C:/Users/disrct/Desktop/nnali/neuralnet/checkpoints/good_model/model9292(95ms).keras"
 model = models.load_model(model_path)
 
-def main():
-    image = cv.imread("C:/Users/disrct/Desktop/nnali/winforms/FPS_0.png")
-    print(image.shape)
-    image = resize_image(image, (480, 270))
-    show(image)
-    sys.stdout.write(predict.run(image, model, save=True))
-    sys.stdout.flush()
+# def main():
+image = cv.imread("C:/Users/disrct/Desktop/nnali/winforms/FPS_0.png")
+print(image.shape)
+image = resize_image(image, (480, 270))
+show(image)
+sys.stdout.write(predict.run(image, model, save=True))
+sys.stdout.flush()
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
--- tests.py
+++ tests.py
@@ -3,12 +3,12 @@ from tensorflow.keras import models
 
 from src.utils import *
 
-model_path = "pynet\checkpoints\good_model\model9292(95ms).keras"
+model_path = "neuralnet\checkpoints\good_model\model9292(95ms).keras"
 model = models.load_model(model_path)
 # print(predict.run("data\\teste2.png", model))
 # print(predict.run("data\\teste.png", model))
 # print(predict.run("data\inquisicao_crop_edit.png", model, save=True))
-print(predict.run("data\inquisicao_crop.png", model, save=True))
+print(predict.run(cv.imread("data\inquisicao_crop.png"), model, save=True))
 
 # image = cv.imread("data\inquisicao_crop_edit.png")
 # image = binary(image)
